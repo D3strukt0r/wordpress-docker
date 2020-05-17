1280170d1be5ea2a84a912aae5b9b6d0ae95abbd
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-05-17 18:40:54 +0200

    Add nginx

--- build-nginx.sh
+++ build-nginx.sh
@@ -3,18 +3,16 @@
 # Output
 set -ex
 
-mkdir /data/web -p
-mv ./web/cpresources /data/web
-ln -s /data/web/cpresources ./web/cpresources
+# Use default config for nginx
+mv /tmp/app/default-nginx.conf /etc/nginx/conf.d/default.conf
 
 # Link wp-config.php
-mv ./wp-config-sample.php /data/wp-config.php
-ln -s /data/wp-config.php ./wp-config.php
+cd /app
+ln -sf /data/wp-config.php ./wp-config.php
 
 # Link wp-content/
-mv ./wp-content /data/wp-content
 ln -s /data/wp-content ./wp-content
 
 # Fix permissions
-chown -R www-data:www-data /app
-chown -R www-data:www-data /data
+chown -R nginx:nginx /app
+chown -R nginx:nginx /data
