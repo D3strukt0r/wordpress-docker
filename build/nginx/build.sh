3bb68126e7ad2767441f1c6ceec1895cbf3c31fb
Manuele Vaccari <manuele.vaccari@gmail.com>
2020-05-23 21:23:45 +0200

    Remove default site.conf

--- build/nginx/build.sh
+++ build/nginx/build.sh
@@ -4,6 +4,7 @@ set -eux
 
 # Use default config for nginx
 mv /build/nginx.conf /etc/nginx/nginx.template
+rm /etc/nginx/conf.d/default.conf
 mv /build/default.conf /etc/nginx/conf.d/default.template
 mv /build/default-ssl.conf /etc/nginx/conf.d/default-ssl.template
 
